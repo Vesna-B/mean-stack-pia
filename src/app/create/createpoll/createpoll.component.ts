5f3eca5f69ce9d7bb6ee6a1e7a098039ae7d2c4d
Vesna-B <babicvesna94@gmail.com>
2020-02-03 19:29:14 +0100

    Forms for creating test and poll are finished

--- src/app/create/createpoll/createpoll.component.ts
+++ src/app/create/createpoll/createpoll.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-createpoll',
@@ -8,14 +8,68 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class CreatepollComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-
+  questionForm: FormGroup;
 
   ngOnInit() {
-
+    this.questionForm = this.formBuilder.group({
+      name: [''],
+      info: [''],
+      startDate: [Date],
+      endDate: [Date],
+      questions: this.formBuilder.array([
+        this.initQuestion()
+      ])
+    });
   }
 
-  
+
+  initQuestion() {
+    return this.formBuilder.group({
+      questionTitle: [],
+      questionType: [],
+      options: new FormArray([
+        this.initOptions()
+      ])
+    });
+  }
+
+  initOptions() {
+    return this.formBuilder.group({
+      optionTitle: ['']
+    });
+  }
+
+
+  addQuestion() {
+    const questionArray = <FormArray>this.questionForm.get('questions');
+    questionArray.push(this.initQuestion());
+  }
+
+  addOption(i) {
+    const optionArray = <FormArray>this.questionForm.get(['questions', i, 'options']);
+    optionArray.push(this.initOptions());
+  }
+
+
+  getQuestions(form) {
+    return form.controls.questions.controls;
+  }
+
+  getOptions(question) {
+    return question.controls.options.controls;
+  }
+
+
+  removeQuestion(i) {
+    const questionArray = <FormArray>this.questionForm.get('questions');
+    questionArray.removeAt(i);
+  }
+
+  removeOption(i, j) {
+    const optionArray = <FormArray>this.questionForm.get(['questions', i, 'options']);
+    optionArray.removeAt(j);
+  }
 
 }
--- src/app/create/createtest/createtest.component.ts
+++ src/app/create/createtest/createtest.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-createtest',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreatetestComponent implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
+  questionForm: FormGroup;
 
   ngOnInit() {
+    this.questionForm = this.formBuilder.group({
+      name: [''],
+      info: [''],
+      startDate: [Date],
+      endDate: [Date],
+      duration: [Number],
+      questions: this.formBuilder.array([
+        this.initQuestion()
+      ])
+    });
+  }
+
+  initQuestion() {
+    return this.formBuilder.group({
+      questionTitle: [],
+      questionType: [],
+      options: new FormArray([
+        this.initOptions()
+      ])
+    });
+  }
+
+  initOptions() {
+    return this.formBuilder.group({
+      optionTitle: ['']
+    });
+  }
+
+
+  addQuestion() {
+    const questionArray = <FormArray>this.questionForm.get('questions');
+    questionArray.push(this.initQuestion());
+  }
+
+  addOption(i) {
+    const optionArray = <FormArray>this.questionForm.get(['questions', i, 'options']);
+    optionArray.push(this.initOptions());
+  }
+
+
+  getQuestions(form) {
+    return form.controls.questions.controls;
+  }
+
+  getOptions(question) {
+    return question.controls.options.controls;
+  }
+
+
+  removeQuestion(i) {
+    const questionArray = <FormArray>this.questionForm.get('questions');
+    questionArray.removeAt(i);
+  }
+
+  removeOption(i, j) {
+    const optionArray = <FormArray>this.questionForm.get(['questions', i, 'options']);
+    optionArray.removeAt(j);
   }
 
 }
