df6bb12cc1feec9b5c9c8959f223e28cdbdf54f8
Vesna-B <babicvesna94@gmail.com>
2020-02-07 15:27:02 +0100

    Final version before exam

--- src/app/userspages/basic/basic.component.ts
+++ src/app/userspages/basic/basic.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AnswerService } from 'src/app/answer/answer.service';
 import { Poll } from 'src/app/answer/poll';
+import { SignupService } from 'src/app/signup/signup.service';
 
 @Component({
   selector: 'app-basic',
@@ -11,7 +12,7 @@ export class BasicComponent implements OnInit {
 
   polls: Poll[] = [];
 
-  constructor(private anwserService: AnswerService) { }
+  constructor(private anwserService: AnswerService, private signupService: SignupService) { }
 
   ngOnInit() {
     this.getPolls();
@@ -28,6 +29,10 @@ export class BasicComponent implements OnInit {
   fillPoll(poll) {
     let fetchedPoll = this.polls.find(({ _id }) => _id === poll._id);
     this.anwserService.fillPoll(fetchedPoll);  
+  }
+
+  logout() {
+    this.signupService.logout();
   }
 
 }
